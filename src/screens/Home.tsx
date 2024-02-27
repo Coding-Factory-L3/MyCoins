7da542de163fa87904de95f327814f1ba7e5ca76
FlorianCohenJoly <floriancohenjoy@gmail.com>
2024-02-27 16:43:33 +0100

    add .env

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,43 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {ActivityIndicator, FlatList, StyleSheet, View} from 'react-native';
+import {useAuth} from '../contexts/AuthContext';
+import {Text} from 'react-native-elements';
+import theme from '../../theme';
+import MainItemBox from '../components/MainPage/MainItemBox';
 
 const Home: React.FC = () => {
+  const {makeApiCall} = useAuth();
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await makeApiCall({
+          method: 'GET',
+          url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en',
+        }).then(response => {
+          setData(response);
+          setLoading(false);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <></>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <FlatList
+            data={data}
+            renderItem={({item}) => <MainItemBox item={item} />}
+          />
+        </>
+      )}
     </View>
   );
 };
--- theme.js
+++ theme.js
@@ -4,5 +4,9 @@ export default {
     secondary: '#D0D2E1',
     background: '#EBECF1',
     tertiary: '#A9A9C5',
+    text: {
+      light: '#EBECF1',
+      dark: '#2D2F3A',
+    }
   },
 };
