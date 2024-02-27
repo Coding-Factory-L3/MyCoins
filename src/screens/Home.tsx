1ddddeb0b88c1d2f73a0335dedf4ce7a4121b580
FlorianCohenJoly <floriancohenjoy@gmail.com>
2024-02-27 16:58:34 +0100

    New colors theme variables

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,58 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  ActivityIndicator,
+  FlatList,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  View,
+} from 'react-native';
+import {useAuth} from '../contexts/AuthContext';
+import {Text} from 'react-native-elements';
+import theme from '../../theme';
+import MainItemBox from '../components/MainPage/MainItemBox';
 
 const Home: React.FC = () => {
+  const {makeApiCall} = useAuth();
+  const [data, setData] = useState<any>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await makeApiCall({
+          method: 'GET',
+          url: 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en',
+        }).then(response => {
+          setData(response);
+          setLoading(false);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <></>
+      {loading ? (
+        <ActivityIndicator size="large" color="#0000ff" />
+      ) : (
+        <>
+          <ScrollView>
+            <Text
+              h1
+              style={{textAlign: 'center', color: theme.colors?.light.primary}}>
+              MyCoin
+            </Text>
+            <TextInput placeholder="Rechercher .." style={styles.search} />
+            <FlatList
+              data={data}
+              renderItem={({item}) => <MainItemBox item={item} />}
+            />
+          </ScrollView>
+        </>
+      )}
     </View>
   );
 };
@@ -13,6 +61,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  search: {height: 40, margin: 12, borderWidth: 1, padding: 10},
 });
 
 export default Home;
--- theme.js
+++ theme.js
@@ -1,8 +1,18 @@
 export default {
   colors: {
-    primary: '#908DB8',
-    secondary: '#D0D2E1',
-    background: '#EBECF1',
-    tertiary: '#A9A9C5',
+    light: {
+      primary: '#908DB8',
+      secondary: '#D0D2E1',
+      background: '#EBECF1',
+      tertiary: '#A9A9C5',
+      text: '#EBECF1',
+    },
+    dark: {
+      primary: '#2B2B2B',
+      secondary: '#3B3B3B',
+      background: '#1C1C1C',
+      tertiary: '#4B4B4B',
+      text: '#EBECF1',
+    },
   },
 };
