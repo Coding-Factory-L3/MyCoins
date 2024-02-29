dacf4749008f6c27b3860c451c17dfc499f19a4f
progresso85 <martin.heuripeau@gmail.com>
2024-02-29 10:17:09 +0100

    :feat: Profile v1

--- src/navigation/AppNavigationStack.tsx
+++ src/navigation/AppNavigationStack.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/Home';
+import Profile from '../screens/Profile';
 
 const Stack = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ const AppStack: React.FC = () => {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Profile" component={Profile} />
     </Stack.Navigator>
   );
 };
