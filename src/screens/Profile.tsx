c245663c2379eadec35caf93c0730ce2e20e8117
progresso85 <71846489+progresso85@users.noreply.github.com>
2024-02-29 15:15:39 +0100

    :feat: Profile v1 (#9)
    Co-authored-by: Rui Gaspar <84150426+justRunnz@users.noreply.github.com>

---
+++ src/screens/Profile.tsx
@@ -0,0 +1,96 @@
+import {SafeAreaView, StyleSheet, TouchableOpacity, View} from 'react-native';
+import React from 'react';
+import {Image, Text} from 'react-native-elements';
+
+const Profile = () => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.background_container}>
+        <Image
+          source={require('../assets/image/profile.jpeg')}
+          style={styles.background_img}
+        />
+      </View>
+
+      <View style={styles.profile_container}>
+        <Image
+          source={require('../assets/image/profile_logo.jpeg')}
+          style={styles.logo}
+        />
+        <Text h3>name</Text>
+        <Text h4>Job</Text>
+        <Text h4>Localisation</Text>
+
+        <View style={styles.rs_container}>
+          <View style={styles.followers_container}>
+            <Text>122</Text>
+            <Text>Followers</Text>
+          </View>
+
+          <View style={styles.followers_container}>
+            <Text>60</Text>
+            <Text>Followings</Text>
+          </View>
+        </View>
+        <View style={styles.btn_container}>
+          <TouchableOpacity style={styles.edit_container}>
+            <Text>Edit Profile</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.edit_container}>
+            <Text>Add Friends</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  background_container: {
+    width: '100%',
+  },
+  background_img: {
+    height: 228,
+    width: '100%',
+  },
+  profile_container: {
+    flex: 1,
+    alignItems: 'center',
+    overflow: 'visible',
+    marginTop: -90,
+  },
+  logo: {
+    height: 155,
+    width: 155,
+    borderRadius: 999,
+    borderWidth: 2,
+  },
+  rs_container: {
+    paddingVertical: 8,
+    flexDirection: 'row',
+  },
+  followers_container: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  btn_container: {
+    flexDirection: 'row',
+  },
+  edit_container: {
+    width: 124,
+    height: 36,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#00F7FF',
+    borderRadius: 10,
+    marginHorizontal: 2,
+  },
+});
+
+export default Profile;
